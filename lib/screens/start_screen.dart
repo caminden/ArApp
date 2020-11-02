7b08c16d25024f5614e6de03760712273e6f8bef
chase minden <caminden@gmail.com>
2020-11-02 15:23:27 -0600

    Start project, test start screen

---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AR App Start screen"),
+      ),
+    );
+  }
+
+}
